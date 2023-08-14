5333128deb9daf0ac5cd87dea5ad71e57e276c84
Noah <noahwinzig@gmail.com>
2023-08-14 11:45:46 -0400

    editing commands for seed all and undo, need to be tested

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,28 +1,24 @@
 from flask.cli import AppGroup
-from .seeds import seed_employees, undo_employees
+from .seeds import seed_employees, undo_employees, seed_offices, undo_offices, seed_shifts, undo_shifts
 
 from app.models.db import db, environment, SCHEMA
 
-# Creates a seed group to hold our commands
-# So we can type `flask seed --help`
 seed_commands = AppGroup('seed')
 
-
-# Creates the `flask seed all` command
+# seed all
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
-        # Before seeding in production, you want to run the seed undo
-        # command, which will  truncate all tables prefixed with
-        # the schema name (see comment in users.py undo_users function).
-        # Make sure to add all your other model's undo functions below
+        undo_shifts()
         undo_employees()
+        undo_offices()
+    seed_offices()
     seed_employees()
-    # Add other seed functions here
+    seed_shifts()
 
-
-# Creates the `flask seed undo` command
+# undo seeds
 @seed_commands.command('undo')
 def undo():
+    undo_shifts()
     undo_employees()
-    # Add other undo functions here
+    undo_offices()
