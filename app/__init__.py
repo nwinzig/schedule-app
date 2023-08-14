124040a351e6f3ce79c7dce1c1ecf086effd244d
Noah <noahwinzig@gmail.com>
2023-08-14 11:21:27 -0400

    editing seeds and adding to gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
-from .models import db, User
+from .models import db, Employee, Shift, Office
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .seeds import seed_commands
@@ -19,7 +19,7 @@ login.login_view = 'auth.unauthorized'
 
 @login.user_loader
 def load_user(id):
-    return User.query.get(int(id))
+    return Employee.query.get(int(id))
 
 
 # Tell flask about our seed commands
---
+++ app/seeds/employees.py
@@ -0,0 +1,26 @@
+from app.models import db, Employee, environment, SCHEMA, Shift, Office
+from sqlalchemy.sql import text
+
+
+# Adds a demo user, you can add other users here if you want
+def seed_employees():
+    demo = Employee(
+        name='Jack Johnson', username='jjohnson', email='demo@aa.io', password='password')
+    demo2 = Employee(
+        name="Marnie O'Hare",username="mO'Hare", email='marnie@aa.io', password='password')
+    demo3 = Employee(
+        name="Bobby Brown", username='bBrown', email='bobbie@aa.io', password='password')
+
+    db.session.add(demo)
+    db.session.add(demo2)
+    db.session.add(demo3)
+    db.session.commit()
+
+
+def undo_users():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM employees"))
+
+    db.session.commit()
