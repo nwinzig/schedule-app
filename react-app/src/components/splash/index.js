ac762f79cdbff7f87b982957a88f7ebb3766ee11
Noah <noahwinzig@gmail.com>
2023-09-13 06:48:35 -0400

    setting up environment to work on a basic front end template

---
+++ react-app/src/components/splash/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import './splash.css'
+
+function SplashPage(){
+    return (
+        <>
+        <h1>Schedule View</h1>
+
+        </>
+    )
+}
+
+export default SplashPage
